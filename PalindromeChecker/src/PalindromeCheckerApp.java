0bba97b1c7494dd4669bf44f57487aec61713891
ShajhinRoe <sr6922@srmist.edu.in>
2026-02-24 16:01:50 +0530

    Palak Added UC2-HardCorePalindrome

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
-        public static void main(String[] args) {
-            System.out.println("Welcome To the Palindrome Checker Management System");
-            System.out.println("Version: 1.0");
-            System.out.println("System Initialised Successfully");
+    public static void main(String[] args) {
+        String word="madam";
+        boolean isPalindrome=true;
+        for(int i=0;i<word.length()/2;i++){
+            if(word.charAt(i) != word.charAt(word.length()-1-i)){
+                isPalindrome=false;
+                break;
+            }
         }
+
+        if (isPalindrome){
+            System.out.println(word+" is a palindrome");
+        }else{
+            System.out.println(word+" is not a palindrome");
+        }
+    }
 }
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HelloApp
 Hello App – Simple Java Hello World Application for Learning Git and GitFlow
+---
+# PalindomeCheckerApp
+// running procedure
+// flow of project
+// objective
+// what are the topic its will be covered
+// which usecase
+// working of use case 2
