f7393515384f58acd257f7541c2386c719b9f2a5
palak-singh01 <palaksingh35584@gmail.com>
2026-03-03 16:42:17 +0530

    UC8: Linked List Based Palindrome Checker

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,34 +1,72 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+class ListNode {
+    char val;
+    ListNode next;
+    ListNode(char val) { this.val = val; }
+}
 
 public class PalindromeCheckerApp {
 
-    public static boolean isPalindrome(String str) {
-        Deque<Character> deque = new ArrayDeque<>();
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() <= 1) return true;
 
-        // Insert characters into deque
-        for (char ch : str.toCharArray()) {
-            deque.addLast(ch);
+        // 1. Convert string to linked list
+        ListNode head = convertToLinkedList(s);
+
+        // 2. Find the middle using Fast and Slow pointers
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        // Compare front and rear
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
+        // 3. Reverse the second half in-place
+        ListNode secondHalfHead = reverseList(slow);
+        ListNode firstHalfHead = head;
 
-            if (front != rear) {
-                return false;
+        // 4. Compare both halves
+        ListNode tempSecond = secondHalfHead;
+        boolean result = true;
+        while (tempSecond != null) {
+            if (firstHalfHead.val != tempSecond.val) {
+                result = false;
+                break;
             }
+            firstHalfHead = firstHalfHead.next;
+            tempSecond = tempSecond.next;
         }
 
-        return true;
+        // (Optional) Restore the list by reversing back
+        reverseList(secondHalfHead);
+
+        return result;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+
+    private ListNode convertToLinkedList(String s) {
+        ListNode dummy = new ListNode('0');
+        ListNode current = dummy;
+        for (char c : s.toCharArray()) {
+            current.next = new ListNode(c);
+            current = current.next;
+        }
+        return dummy.next;
     }
 
     public static void main(String[] args) {
-        String input = "madam";
-        System.out.println("Is Palindrome? " + isPalindrome(input));
+        PalindromeCheckerApp checker = new PalindromeCheckerApp();
+        String test = "racecar";
+        System.out.println("Is '" + test + "' a palindrome? " + checker.isPalindrome(test));
     }
 }
-
-
-
